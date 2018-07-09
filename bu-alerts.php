80f8dca1dda0bc6d4a3f20249414ccd5a249125e
Anton Kachurin <kachurinanton@gmail.com>
2018-07-09 17:30:59 -0400

    Version bump to 2.4.1

--- bu-alerts.php
+++ bu-alerts.php
@@ -3,7 +3,7 @@
 Plugin Name: BU Alert
 Description: Displays & stores BU Alert emergency messages
 Author: Boston University (IS&T)
-Version: 2.4.0
+Version: 2.4.1
 Author URI: http://www.bu.edu/
 */
 
